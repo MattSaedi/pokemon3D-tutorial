051ad563ed303dc7c78af37498b2641832f42b71
Matt Saedi <mahdiisaedii@gmail.com>
2021-06-05 17:47:20 +0430

    Tracking multiple pokemons

--- Poke 3D/ViewController.swift
+++ Poke 3D/ViewController.swift
@@ -20,7 +20,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.delegate = self
         
         // Show statistics such as fps and timing information
-        sceneView.showsStatistics = true
+//        sceneView.showsStatistics = true
         
         sceneView.autoenablesDefaultLighting = true
     }
@@ -33,7 +33,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         if let imageToTrack = ARReferenceImage.referenceImages(inGroupNamed: "Pokemon Cards", bundle: Bundle.main){
             configuration.trackingImages = imageToTrack
-            configuration.maximumNumberOfTrackedImages = 1
+            configuration.maximumNumberOfTrackedImages = 2
             print("Images Successfully Added")
         }
         // Run the view's session
@@ -60,12 +60,22 @@ class ViewController: UIViewController, ARSCNViewDelegate {
             let planeNode = SCNNode(geometry: plane)
             planeNode.eulerAngles.x = -.pi/2
             node.addChildNode(planeNode)
-            if let pokeScene = SCNScene(named: "art.scnassets/eevee.scn"){
-                if let pokeNode = pokeScene.rootNode.childNodes.first{
-                    pokeNode.eulerAngles.x = .pi/2
-                    planeNode.addChildNode(pokeNode)
+            if imageAnchor.referenceImage.name == "eevee-card"{
+                if let pokeScene = SCNScene(named: "art.scnassets/eevee.scn"){
+                    if let pokeNode = pokeScene.rootNode.childNodes.first{
+                        pokeNode.eulerAngles.x = .pi/2
+                        planeNode.addChildNode(pokeNode)
+                    }
+                }
+            }else if imageAnchor.referenceImage.name == "oddish-card"{
+                if let pokeScene = SCNScene(named: "art.scnassets/oddish.scn"){
+                    if let pokeNode = pokeScene.rootNode.childNodes.first{
+                        pokeNode.eulerAngles.x = .pi/2
+                        planeNode.addChildNode(pokeNode)
+                    }
                 }
             }
+            
         }
         
         return node
